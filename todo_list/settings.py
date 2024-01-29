77cd450fc023a55eca0173ea632cd757da9b03aa
gisele-camargo <gisele.lima.camargo@gmail.com>
2024-01-28 21:45:31 -0300

    .

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#=%a2(ryr!*ri+q=b^t9r29u*crt#wwg#$)9iaf&hr2yu-u27g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = [ '127.0.0.1', '.vercel.app', '177.51.64.128', 'localhost', 'localhost:8080' ]
 
 
 # Application definition
